4231e97cae1ebd7258b4e9460f8e036ba5a2ae51
abdullah almasri <abdullahalmasri374@gmail.com>
2023-07-07 20:45:42 +0300

    services

--- src/main/java/com/company/rentCar/service/CarServiceImp.java
+++ src/main/java/com/company/rentCar/service/CarServiceImp.java
@@ -1,4 +1,52 @@
 package com.company.rentCar.service;
 
-public class CarServiceImp {
+import com.company.rentCar.data.CarDTO;
+import com.company.rentCar.mapper.CarMapper;
+import com.company.rentCar.sql.CarRepository;
+import io.vertx.core.Future;
+import org.mapstruct.factory.Mappers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CarServiceImp implements CarService{
+  private final CarRepository repository;
+
+  public CarServiceImp(CarRepository repository) {
+    this.repository = repository;
+  }
+
+  @Override
+  public Future<List<CarDTO>> findCars() {
+    List<CarDTO> result = new ArrayList<>() ;
+    repository.findAllCar().result().forEach(car ->
+      result.add(Mappers.getMapper(CarMapper.class).carToCarDTO(car))
+    );
+    return Future.succeededFuture(result);
+  }
+
+  @Override
+  public Future<CarDTO> findCarById(UUID carId) {
+    CarDTO carDTO =Mappers.getMapper(CarMapper.class).carToCarDTO(repository.findCarById(carId).result());
+    return Future.succeededFuture(carDTO);
+  }
+
+  @Override
+  public Future<CarDTO> saveCar(CarDTO carDTO) {
+    repository.saveCar(Mappers.getMapper(CarMapper.class).carDTOToCar(carDTO));
+    return Future.succeededFuture(carDTO);
+  }
+
+  @Override
+  public Future<CarDTO> updateCar(CarDTO carDTO) {
+    repository.updateCar(Mappers.getMapper(CarMapper.class).carDTOToCar(carDTO));
+    return Future.succeededFuture(carDTO);
+  }
+
+  @Override
+  public Future<Void> deleteCar(UUID carId) {
+    repository.deleteCar(carId);
+    return Future.succeededFuture();
+  }
 }
--- src/main/java/com/company/rentCar/service/CustomerServiceImp.java
+++ src/main/java/com/company/rentCar/service/CustomerServiceImp.java
@@ -1,4 +1,55 @@
 package com.company.rentCar.service;
 
-public class CustomerServiceImp {
+import com.company.rentCar.data.CustomerDTO;
+import com.company.rentCar.mapper.CustomerMapper;
+import com.company.rentCar.sql.CustomerRepository;
+import io.vertx.core.Future;
+import org.mapstruct.factory.Mappers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CustomerServiceImp implements CustomerService{
+
+  private final CustomerRepository repository;
+
+  public CustomerServiceImp(CustomerRepository repository) {
+    this.repository = repository;
+  }
+
+  @Override
+  public Future<List<CustomerDTO>> findCustomers() {
+    List<CustomerDTO> customers = new ArrayList<>();
+    repository.findAllCustomer().result().forEach(customer ->
+      customers.add(Mappers.getMapper(CustomerMapper.class).customerToCustomerDTO(customer))
+    );
+    return Future.succeededFuture(customers);
+  }
+
+  @Override
+  public Future<CustomerDTO> findCustomerById(UUID customerId) {
+    CustomerDTO dto = Mappers.getMapper(CustomerMapper.class).
+      customerToCustomerDTO(repository.findCustomerById(
+      customerId).result());
+    return Future.succeededFuture(dto);
+  }
+
+  @Override
+  public Future<CustomerDTO> saveCustomer(CustomerDTO customerDTO) {
+    repository.saveCustomer(Mappers.getMapper(CustomerMapper.class).customerDTOToCustomer(customerDTO));
+    return Future.succeededFuture(customerDTO);
+  }
+
+  @Override
+  public Future<CustomerDTO> updateCustomer(CustomerDTO customerDTO) {
+    repository.updateCustomer(Mappers.getMapper(CustomerMapper.class).customerDTOToCustomer(customerDTO));
+    return Future.succeededFuture(customerDTO);
+  }
+
+  @Override
+  public Future<Void> deleteCustomer(UUID customerId) {
+    repository.deleteCustomer(customerId);
+    return Future.succeededFuture();
+  }
 }
