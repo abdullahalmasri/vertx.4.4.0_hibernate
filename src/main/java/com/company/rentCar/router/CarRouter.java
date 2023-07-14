892826b37e50e9e12466bcf83bcf1a1836c0434a
abdullah almasri <33969555+abdullahalmasri@users.noreply.github.com>
2023-07-14 04:16:49 +0300

    Update CarRouter.java

--- src/main/java/com/company/rentCar/router/CarRouter.java
+++ src/main/java/com/company/rentCar/router/CarRouter.java
@@ -11,7 +11,7 @@ import static com.company.rentCar.Constrant.ConstrantQuery.APPLICATION_JSON;
 /**
  * The type Car router.
  */
-public class CarRouter extends AbstractRouter{
+public class CarRouter implements AbstractRouter{
   private final CarHandler handler;
   private final Vertx vertx;
 
@@ -31,6 +31,7 @@ public class CarRouter extends AbstractRouter{
    *
    * @param router the router
    */
+@Override 
   public void setRouter(Router router) {
     router.mountSubRouter("/api/v1", buildBookRouter());
   }
